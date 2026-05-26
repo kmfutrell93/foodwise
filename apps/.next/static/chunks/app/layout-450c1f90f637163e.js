d04853c3e3814cfa4e6fe351305b90efc00972e4
Kenneth Futrell <kmfutrell93@gmail.com>
2026-05-26 09:40:03 -0400

    fix: remove .next from git, update gitignore

--- apps/.next/static/chunks/app/layout-450c1f90f637163e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5565:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,9361,23)),Promise.resolve().then(a.t.bind(a,9324,23))},9324:()=>{},9361:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_5c4a2f"}}},e=>{var s=s=>e(e.s=s);e.O(0,[720,441,684,358],()=>s(5565)),_N_E=e.O()}]);
